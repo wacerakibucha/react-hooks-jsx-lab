ed9dd71e0296aa4b27c676532224dc930248c5c3
wacerakibucha <beatricekibucha@gmail.com>
2025-10-22 20:12:55 +0300

    added changes

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I love coding and building amazing web applications!</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
